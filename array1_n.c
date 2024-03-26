f22ff27fadccd9a4d9ba9f7b08342a76a9af6d67
Tushar Saini <145689633+tusharsaini590@users.noreply.github.com>
2024-03-27 05:11:02 +0530

    Update array1_n.c

--- array1_n.c
+++ array1_n.c
@@ -1,9 +1,10 @@
 // C program to create dynamic array
 // without function without pointer
+
 #include "stdio.h"  // includibg header file(important)
 int main() {
     int size; // initializing global variables
-    int newarray[size];
+    int newarray[size]; // declaring array variable with size as variable size 
 
     printf("Enter size of array: "); //built in function to print on console
     scanf("%d", &size);              // built in function to read data written on console
@@ -11,7 +12,7 @@ int main() {
 
 
     printf("Enter %d elements for the array", size);
-    for (int i = 0; i < size; ++i)      // iteration loop
+    for (int i = 0; i < size; ++i)      // iteration loop (**imp:- "i<size" -yes, "i<=size"- no; bcz then we have to put i+1 elements to print i elements
     {
         scanf("%d", &newarray[i]);
        }
