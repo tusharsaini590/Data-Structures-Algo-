c8dd5bbcd4837248b716f2345eddcddb6715d5c0
Tushar Saini <145689633+tusharsaini590@users.noreply.github.com>
2024-03-27 05:27:39 +0530

    Update array2_f.c

--- array2_f.c
+++ array2_f.c
@@ -1,20 +1,20 @@
 // C program to create dynamic array
 // with function
 
-#include <stdio.h>
+#include <stdio.h>   // headerfile containing imp prebuilt functions like scanf,printf, sprintf, gets etc
 
 // Function to read the size of the array
 int readArraySize() {
-    int size;
+    int size;   //local variable to this function only
     printf("Enter size of array: ");
     scanf("%d", &size);
     return size;
 }
 
 // Function to read array elements
-void readArrayElements(int arr[], int size) {
+void create_array(int arr[], int size) {
     printf("Enter %d elements for the array: ", size);
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i) { 
         scanf("%d", &arr[i]);
     }
 }
@@ -25,14 +25,15 @@ void displayArray(int arr[], int size) {
     for (int i = 0; i < size; ++i) {
         printf("%d ", arr[i]);
     }
-    printf("\n");
+    printf("\n");  //\n : a new line character
 }
 
+// Main function starts from here
 int main() {
     int size;
-    size = readArraySize();
+    size = readArraySize();  // output from readArraySize stores in size variable declared in the main function's block 
     int newarray[size];
-    readArrayElements(newarray, size);
+    create_array(newarray, size);
     displayArray(newarray, size);
 
     return 0;
